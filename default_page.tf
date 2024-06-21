2a613ec57bcb9f00f21c232080c0283882e13a7f
Matthew Baggett <matthew@baggett.me>
2024-06-21 16:48:12 +0200

    Initial Commit

---
+++ default_page.tf
@@ -0,0 +1,19 @@
+locals {
+  default_page = "<h1>Hello, World!</h1>"
+}
+resource "docker_config" "default_page" {
+  name = "${var.service_name}.index.html-${substr(sha1(local.default_page), 0, 4)}"
+  data = base64encode(local.default_page)
+}
+resource "local_file" "default_page" {
+  content  = base64decode(docker_config.default_page.data)
+  filename = "${path.root}/.debug/nginx/index.html"
+}
+resource "docker_config" "default_conf" {
+  name = "${var.service_name}.default.conf-${substr(sha1(file("${path.module}/default.conf")), 0, 4)}"
+  data = base64encode(file("${path.module}/default.conf"))
+}
+resource "local_file" "default_conf" {
+  content  = base64decode(docker_config.default_conf.data)
+  filename = "${path.root}/.debug/nginx/default.conf"
+}
